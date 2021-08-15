e1537e8a3f2970297d00bcdcd9c57879984f1084
MustafaCOKER <mustafacoker@yandex.com>
2021-08-15 18:32:30 +0300

    main's args size checked

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 #include "DiffFinder.hpp"
 
+void printUsage()
+{
+    std::cout << "USAGE :\n ./DiffFinder path_to_another_image\n"; 
+}
+
 int main(int argc, char** argv)
 {
+    if (argc < 2) {
+        printUsage();
+        exit(-1);
+    }
+
     DiffFinder *finderInstance = DiffFinder::getInstance();
 
     finderInstance->sayHi();
